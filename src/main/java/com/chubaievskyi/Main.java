9928d3d49821eb3be9dd2132191f2548cbcac9d3
Pavlo Chubaievskyi <chubaievskyi@gmail.com>
2023-11-21 11:22:30 +0200

    first commit

---
+++ src/main/java/com/chubaievskyi/Main.java
@@ -0,0 +1,16 @@
+package com.chubaievskyi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Program start!");
+        LOGGER.info("End of program!");
+
+    }
+}
